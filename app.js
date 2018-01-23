263275e022fbab1882eb82cfbd0d39625f26cf51
Andy <amillion3@gmail.com>
2018-01-22 21:58:29 -0600

    another attempt to fix heroku app crash issue

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 const routes = require('./router');
 
 // Define the hostname and port where the server can be found
-// const hostname = "127.0.0.1";
-// const port = 3000;
+const hostname = "127.0.0.1";
+const port = process.env.PORT || 3000;
 
 // Define the directory where static files are found
 app.use(express.static('public'));
@@ -20,3 +20,16 @@ app.listen(port, () => {
   // Display server location information to the console
   console.log(`Server is listening at http://${hostname}:${port}/`);
 })
+
+
+
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
