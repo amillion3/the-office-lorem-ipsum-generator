afb9ab28ed20544dfc12b47f1f7dd25d844006c0
Andy <amillion3@gmail.com>
2018-01-22 22:00:48 -0600

    another try

--- app.js
+++ app.js
@@ -20,16 +20,3 @@ app.listen(port, () => {
   // Display server location information to the console
   console.log(`Server is listening at http://${hostname}:${port}/`);
 })
-
-
-
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
