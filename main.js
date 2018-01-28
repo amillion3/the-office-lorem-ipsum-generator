2bb2e6d00544e9fefd8845dc31c92969a345225d
Andy <amillion3@gmail.com>
2018-01-27 21:18:51 -0600

    closer still...

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-function displayLoremTextBox() {
-    // generated-text.style.display="block";
-    document.getElementById("generated-text").style.display="initial";
+// function displayLoremTextBox() {
+//     // generated-text.style.display="block";
+//     document.getElementById("generated-text").style.display="block";
+// }
+
+// Get the modal
+var loremDiv = document.getElementById("generated-text");
+
+// Get the button that opens the modal
+var btn = document.getElementById("generate-button");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    loremDiv.style.display = "block";
 }
