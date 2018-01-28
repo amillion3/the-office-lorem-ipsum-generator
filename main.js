ea0dba2a35099eade8cd4f3c8286e0fe885122a2
Andy <amillion3@gmail.com>
2018-01-27 21:09:21 -0600

    closer still

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function displayLoremTextBox() {
     // generated-text.style.display="block";
-    document.getElementById("generated-text").style.display="block";
+    document.getElementById("generated-text").style.display="initial";
 }
