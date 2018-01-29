67b9932e8e009108aee14dd222862e390c42ace1
Andy <amillion3@gmail.com>
2018-01-28 20:43:04 -0600

    trying a lifted corner box shadow

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-var button = document.getElementById("button");
-button.onclick = function(e) {
-    e.preventDefault();
-    console.log("clicked")
-    var div1 = document.getElementsByClassName("generated-text");
-    var div2 = document.getElementsByClassName("placeholder-div");
-    console.log(div1);
-    console.log(div1);
-    div1[0].style.display = "block";
-    div2[0].style.display = "block";
-};
+// var button = document.getElementById("button");
+
+// button.onclick = function(e) {
+//     e.preventDefault();
+//     console.log("clicked")
+//     var div1 = document.getElementsByClassName("generated-text");
+//     var div2 = document.getElementsByClassName("placeholder-div");
+//     console.log(div1);
+//     console.log(div1);
+//     div1[0].style.display = "block";
+//     div2[0].style.display = "block";
+// };
