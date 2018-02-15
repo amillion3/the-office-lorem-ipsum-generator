2715b13dd5bcf2445eaae7458a4bc202000d1914
Andy <amillion3@gmail.com>
2018-02-14 21:07:26 -0600

    working on hiding the div

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-// var button = document.getElementById("button");
+var button = document.getElementById("button");
 
-// button.onclick = function(e) {
-//     e.preventDefault();
-//     console.log("clicked")
-//     var div1 = document.getElementsByClassName("generated-text");
-//     var div2 = document.getElementsByClassName("placeholder-div");
-//     console.log(div1);
-//     console.log(div1);
-//     div1[0].style.display = "block";
-//     div2[0].style.display = "block";
-// };
+button.onclick = function(e) {
+    e.preventDefault();
+    console.log("clicked");
+    var div1 = document.getElementById("lorem-ipsum");
+    var div2 = document.getElementsByClassName("placeholder-div")[0];
+    console.log(div1);
+    console.log(div2);
+    div1.style.display = "flex";
+    div2.style.display = "flex";
+};
+
+
+// var innerContents = document.getElementsByClassName("generated-text")[0].innerHTML;
+//     if (!innerContents.includes("<p>")) {
+//       $('.copy-button').hide();
+//       document.getElementsByClassName("generated-text")[0].style.display = "none";
+//     };
+//     var clip = new Clipboard('.copy-button');
+//     clip.on('success', function(e) {
+//       $('#copyOverlay').show();
+//       $('#copyOverlay').fadeOut(2000);
+//       e.clearSelection();
+//     });
